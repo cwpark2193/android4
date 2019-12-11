b6ba45dd0dbf3ebd60bd73ba93eb7c99e3a80119
Istaria <cwpark2193@naver.com>
2019-12-11 09:36:47 +0900

    1 f1.txt 생성

---
+++ android3/hello.java
@@ -0,0 +1,8 @@
+public class hello extends AppCompatActivity {
+
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+        }
+
+}
